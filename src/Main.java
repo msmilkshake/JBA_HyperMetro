fdf21d89d10e0ef8c9b3b21a6f7f3fa24eb3d7e0
Paulo Filipe <prnfilipe@gmail.com>
2020-08-24 11:57:43 +0100

    Stage 3 completed

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,7 @@ import metro.ui.TextUI;
 import java.util.Scanner;
 
 public class Main {
-    
     public static void main(String[] args) {
         new TextUI(args, new Scanner(System.in)).start();
-    }
-    
-}
-
-class Test {
-    public static void main(String[] args) {
-        System.out.println("Put test code in here!!");
     }
 }
